179fb0bc96754f20f7722a56cf794fab123d31e4
Katzx3 <48260067+Katzx3@users.noreply.github.com>
2021-09-24 19:21:47 -0600

    css y formato

---
+++ main.cc
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Página Log in</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        
+        <link rel="stylesheet" type="text/css" href="main.css">
+        
+	</head>
+    
+	<body>
+    
+    <!-- As a heading -->
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Banco Pokemon</span>
+  </div>
+</nav>
+        
+        <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4">
+                <div class="login-form bg-light mt-4 p-4">
+                    <form action="" method="" class="row g-3">
+                        <h4>Bienvenido Ingrese Su Contraseña</h4>
+                        <div class="col-12">
+                            <label>Contraseña</label>
+                            <input type="password" name="password" class="form-control" placeholder="Password">
+                        </div>
+                      
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                        <button type="button" class="btn btn-primary">1</button>
+                                </div>
+                                <div class="col">
+                                       <button type="button" class="btn btn-primary">2</button>
+                                </div>
+                                <div class="col">
+                                       <button type="button" class="btn btn-primary">3</button>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                        <button type="button" class="btn btn-primary">3</button>
+                                </div>
+                                <div class="col">
+                                       <button type="button" class="btn btn-primary">4</button>
+                                </div>
+                                <div class="col">
+                                        <button type="button" class="btn btn-primary">5</button> 
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                        <button type="button" class="btn btn-primary">6</button>
+                                </div>
+                                <div class="col">
+                                      <button type="button" class="btn btn-primary">7</button>
+                                </div>
+                                <div class="col">
+                                        <button type="button" class="btn btn-primary">8</button> 
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                       <button type="button" class="btn btn-primary">*</button>
+                                </div>
+                                <div class="col">
+                                      <button type="button" class="btn btn-primary">9</button>
+                                </div>
+                                <div class="col">
+                                        <button type="button" class="btn btn-primary">#</button> 
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-dark float-end">Login</button>
+                        </div>
+                    </form>
+                    <hr class="mt-4">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                        <button type="button" class="btn btn-primary">Menú Principal</button>
+                </div>
+            <div class="col">
+                        <button type="button" class="btn btn-primary">Volver Atrás</button>
+            </div>
+            <div class="col">
+                        <button type="button" class="btn btn-primary">Salir</button>
+            </div> 
+      
+            </div>
+        </div>
+
+        
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+	</body>
+	
+</html>
+    
+   
